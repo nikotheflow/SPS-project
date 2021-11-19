d696e6a613437c080156f1ef3c3c20ae1c325664
nikotheflow <bboyfoxxx@yandex.ru>
2021-11-19 19:09:05 +0300

    Add interactive buttons, tabbar to question pages

--- script/script.js
+++ script/script.js
@@ -1,24 +1,56 @@
-//tabbar
-let tabbar = document.querySelector('.tabbar');
-let tabbarList = document.querySelectorAll('.tabbar_item');
+document.addEventListener('DOMContentLoaded', function() {
+  //tabbar
+  let tabbarList = document.querySelectorAll('.tabbar_item');
 
-tabbarList.forEach((link) => link.addEventListener("click", listActive));
+  tabbarList.forEach((link) => link.addEventListener('click', listActive));
 
-function listActive() {
-  tabbarList.forEach((link) => 
-  link.classList.remove("tabbar_item_active"));
-  this.classList.add("tabbar_item_active");
-};
-
-//test-page
-let answerField = document.querySelector('.answer-field');
-let answerButton = document.querySelector('.answer-button');
-
-answerField.addEventListener('input', activatePrimaryButton);
-
-function activatePrimaryButton() {
-  answerButton.classList.remove('primary-button_disable');
-  if (answerField.value == "") {
-    answerButton.classList.add('primary-button_disable');
+  function listActive() {
+    tabbarList.forEach((link) => 
+    link.classList.remove("tabbar_item_active"));
+    this.classList.add("tabbar_item_active");
   };
-};
+
+  //test-page
+  let answerInputField = document.querySelector('.answer-input-field');
+  let nextQuestionButton = document.querySelector('.next-question-button');
+  let radioButtonList = document.querySelectorAll('.radio-button');
+  let completeTestButton = document.querySelector('.complete-test-button');
+
+  if (answerInputField) {
+    answerInputField.value = '';
+    answerInputField.addEventListener('input', activatePrimaryButton);
+    nextQuestionButton.addEventListener('click', nextQuestion);
+  };
+
+  if (completeTestButton) {
+    completeTestButton.addEventListener('click', completeTest);
+  };
+
+  function activatePrimaryButton() {
+    nextQuestionButton.classList.remove('primary-button_disable');
+    if (answerInputField.value == '') {
+      nextQuestionButton.classList.add('primary-button_disable');
+    };
+  };
+
+  function nextQuestion() {
+    if (this.classList.contains('primary-button_disable') == false) {
+      document.location.href = 'test-last.html';
+    };  
+  };
+
+  function completeTest() {
+    if (this.classList.contains('primary-button_disable') == false) {
+      document.location.href = 'index.html';
+    };  
+  }; 
+  
+  radioButtonList.forEach((radio) => radio.addEventListener('click', clickRadioButton));
+
+  function clickRadioButton() {
+    radioButtonList.forEach((radio) => 
+    radio.classList.remove('radio-button_active'));
+    this.classList.add("radio-button_active");
+    completeTestButton.classList.remove('primary-button_disable');
+  };
+});
