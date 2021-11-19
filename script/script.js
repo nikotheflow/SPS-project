65560900d6909a06eadc0d40e301fb04cefab7d7
nikotheflow <bboyfoxxx@yandex.ru>
2021-11-19 14:37:52 +0300

    Add test page, first qustion, interactive button

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+//tabbar
 let tabbar = document.querySelector('.tabbar');
 let tabbarList = document.querySelectorAll('.tabbar_item');
 
@@ -7,4 +8,17 @@ function listActive() {
   tabbarList.forEach((link) => 
   link.classList.remove("tabbar_item_active"));
   this.classList.add("tabbar_item_active");
-}
+};
+
+//test-page
+let answerField = document.querySelector('.answer-field');
+let answerButton = document.querySelector('.answer-button');
+
+answerField.addEventListener('input', activatePrimaryButton);
+
+function activatePrimaryButton() {
+  answerButton.classList.remove('primary-button_disable');
+  if (answerField.value == "") {
+    answerButton.classList.add('primary-button_disable');
+  };
+};
